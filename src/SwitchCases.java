f5a5bb2fc4cec2a6a75ae86da56f77a9027a0723
Kunal <kunalkashyap0754@gmail.com>
2026-06-15 17:35:30 +0530

    switch cases

--- src/SwitchCases.java
+++ src/SwitchCases.java
@@ -7,12 +7,30 @@ void main() {
      int age;
      System.out.print("Enter your age :");
      age = sc.nextInt();
-     if (age>56){
-          System.out.println("You are experienced");
-     } else if (age >46) {
-          System.out.println("You are semi experienced");
+//     if (age>56){
+//          System.out.println("You are experienced");
+//     } else if (age >46) {
+//          System.out.println("You are semi experienced");
+//     }
+//     else{
+//          System.out.println("You are not experienced.");
+//     }
+     //Switch Case
+     switch (age){
+          case 18:
+               System.out.println("You are legally an adult.");
+               break;
+
+          case 23:
+               System.out.println("You are going to join a Job");
+               break;
+
+          case 60:
+               System.out.println("You are gonna retired.");
+               break;
+          default:
+               System.out.println("Enjoy the moment");
+
      }
-     else{
-          System.out.println("You are not experienced.");
-     }
+
 }
