a2e8b5c29da40ef0dada736ecc85ad46f039e98e
Kunal <kunalkashyap0754@gmail.com>
2026-06-17 14:36:55 +0530

    Some changes in logics

--- src/PracticeGame.java
+++ src/PracticeGame.java
@@ -6,21 +6,21 @@ void main() {
     //ROCK , PAPER AND SCISSORS Game
     Scanner sc = new Scanner(System.in);
     Random rd = new Random();
-    System.out.println("Choose Rock (1), paper(2), scissor(3):");
+    System.out.println("Choose Rock (0), paper(1), scissor(2):");
     int choice = sc.nextInt();
-    int answer = rd.nextInt(3);
+    int answer = rd.nextInt(2);
     switch (choice){
         case 1 -> System.out.println(answer);
         case 2 -> System.out.println(answer);
         case 3 -> System.out.println(answer);
     }
-    if (choice == 1 && answer == 3){
+    if (choice == 0 && answer == 2){
         System.out.println("Rock v Scissor -> WIN");
 
-    } else if (choice == 2 && answer == 1) {
+    } else if (choice == 1 && answer == 0) {
         System.out.println(" Paper v Rock -> Win");
 
-    } else if (choice == 3 && answer == 2) {
+    } else if (choice == 2 && answer == 1) {
         System.out.println(" Scissor v Paper -> Win");
     } else {
         System.out.println("Better luck next time!");
