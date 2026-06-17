7bf88478bc737457dc1bd90a4ff73068fe30ea32
Kunal <kunalkashyap0754@gmail.com>
2026-06-17 22:02:23 +0530

    while loops

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -3,6 +3,9 @@
 
 void main() {
     //WHILE LOOPS
+    /*
+     Checks the condition first & then executes the code
+     */
     int a = 1;
     while (a <= 3) {
         System.out.println(a);
---
+++ src/doWhileLoop.java
@@ -0,0 +1,15 @@
+void main() {
+   //do-while loop
+    /* do-while me pehle do{} execute hoga and then wo check
+    karega while() condition
+
+    meaning- Executes the code first & then checks the condition
+     */
+
+    int a = 10;
+   do {
+       System.out.println(a);
+       a++;
+   }while(a <5);
+
+}
