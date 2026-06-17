00235065c98dd2e4b3e87d74640cf9113ad29dd2
Kunal <kunalkashyap0754@gmail.com>
2026-06-17 23:08:04 +0530

    forLoops

--- src/forLoop.java
+++ src/forLoop.java
@@ -1,4 +1,23 @@
 void main() {
     //for Loop
+//    for (int i = 1; i <= 10; i++) {
+//        System.out.println(i); }
 
+        // 2n = Even Numbers = 0,2,4,6,8
+        // 2n+1 = Odd Numbers = 1,3,5,7,9
+
+    // ODD
+    int n = 5;
+    for (int i = 0; i < n; i++) {
+        System.out.println(2*i+1);
+
+    }
+
+    //EVEN
+    System.out.println( "\n");
+    int x = 10;
+    for (int i = 0; i <n ; i++) {
+        System.out.println(2*i);
+
+    }
 }
