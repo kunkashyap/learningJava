658a42a57157f591f319a421f4f7a86bbab2381c
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-18 20:02:24 +0530

    practice

---
+++ src/PracticeLoops.java
@@ -0,0 +1,26 @@
+void main() {
+    //Q1.
+//    int n = 4;
+//    for (int i = n; i > 0 ; i--) {
+//        for (int j = 0; j < i; j++) {
+//            System.out.print("*");
+//        }
+//        System.out.println("\n");
+//    }
+    // Q2.
+//    int sum = 0 ;
+//    int n = 3;
+//    for (int i = 0; i <= n ; i++) {
+//        sum = sum + (2*i);
+//    }
+//    System.out.print("The sum of even numbers is : ");
+//    System.out.println(sum);
+
+    //Q3. Multiplication Table
+    int n = 5;
+    for (int i = 1; i <= 10; i++) {
+        System.out.printf("%d x %d = %d \n", n , i, n*i);
+
+    }
+
+}
