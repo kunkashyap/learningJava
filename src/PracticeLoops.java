8a7423b1bdc1ed3e99ebd4e8c6991bbd5fa93814
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-18 20:13:29 +0530

    practice

--- src/PracticeLoops.java
+++ src/PracticeLoops.java
@@ -25,9 +25,40 @@ void main() {
 
 
     //Q4. Reverse Multiplication
-    int n = 10;
-    for (int i = 10; i >= 1; i--) {
+//    int n = 10;
+//    for (int i = 10; i >= 1; i--) {
+//        System.out.printf("%d x %d = %d \n", n , i, n*i);
+//
+//    }
+
+
+    //Q6. FACTORIAL USING WHILE LOOP
+    // FACTORIAL n = n* n-1 * n-2 ...
+//    int n = 5;
+//    int i = 1;
+//    int factorial = 1;
+//    while (i<n){
+//            factorial *= i;
+//            i++;
+//    }
+//    System.out.println("Factorial is : " + factorial);
+
+
+    // Q9.
+    int n = 8;
+    int sum = 0;
+    for (int i = 1; i <= 10; i++) {
         System.out.printf("%d x %d = %d \n", n , i, n*i);
+        sum += n*i;
+        System.out.println("Sum of table 8 is:" + sum);
 
     }
+
+
+
+
+
+
+
+
 }
