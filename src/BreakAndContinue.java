26ff247fb524dad3f1dd2e4d59047c74296659e2
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-18 12:40:25 +0530

    breakStatements

--- src/BreakAndContinue.java
+++ src/BreakAndContinue.java
@@ -1,3 +1,41 @@
+
+
 void main() {
-    
+    //Break and Continue Statements using for Loops
+
+//    for (int i = 0; i < 15; i++) {
+//        System.out.println("java is great " + i);
+//        if (i == 8) {
+//            System.out.println("Ending Loop");
+//            break;
+//        }
+//    }
+
+
+    //Break using While Loop
+    int i = 0;
+    while (i < 5){
+        System.out.println("Java is great " + i);
+        i++;
+        if (i == 3){
+            System.out.println("Ending Loop");
+            break;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
