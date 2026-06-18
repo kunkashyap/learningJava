fcad06ad8411e9e6616bb17cdf267495a47fdf39
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-18 20:33:27 +0530

    arrays

---
+++ src/Arrays.java
@@ -0,0 +1,22 @@
+
+
+void main() {
+    // Arrays
+
+    /*
+    Classroom of 500 students - You have to store marks of these 500 students
+    You have 2 options:
+        1. Create variables 500 times
+        2. Use Arrays to store them in one block
+
+     */
+
+    //MEMORY ALLOCATION OF THE ARRAY
+    int [] marks = new int[500];
+
+
+
+
+
+
+}
