80ea980244ed917e402f1129983fd2e7d1ee0888
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-19 13:19:16 +0530

    arrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -11,8 +11,10 @@ void main() {
 
      */
 
-    //MEMORY ALLOCATION OF THE ARRAY -> new int [];
-//    int [] marks = new int[5]; // Declaration + Memory Allocation
+    //MEMORY ALLOCATION OF THE ARRAY
+
+    // Declaration + Memory Allocation
+//    int [] marks = new int[5];
 //    marks[0] = 100;
 //    marks[1] = 50;
 //    marks[2] = 30;
@@ -23,13 +25,18 @@ void main() {
 //    System.out.println(marks[4]);
 //
 
+    //Declaration + Initialize
+//    int [] marks1 = {100,40,50,30};
+//    System.out.println(marks1[2]);
+//
+//
+//    //Declaration and then Memory Allocation
+//    int[] marks2;
+//    marks2 = new int[10];
 
-    int [] marks1 = {100,40,50,30};//Declaration + Initialize
-    System.out.println(marks1[2]);
 
-
-
-
+    // Array indices starts from  0 and goes till (n-1) ,
+    // where n is the size of Array
 
 
 }
