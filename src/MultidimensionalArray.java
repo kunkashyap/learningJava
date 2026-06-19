b9c1350860d53dd521735e13e38b8853092a61f4
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-19 21:09:05 +0530

    Multidimensional Array 2d array

--- src/MultidimensionalArray.java
+++ src/MultidimensionalArray.java
@@ -1,3 +1,25 @@
 void main() {
-    //Nested Array
+    //Multidimensional Array
+    //Contains Arrays in Arrays
+
+
+    //2D Array
+    int [] [] flats;
+    flats = new int[2][3]; //2 rows & 3 columns
+    flats [0] [0] = 101;
+    flats [0] [1] = 102;
+    flats [0] [2] = 103;
+    flats [1] [0] = 201;
+    flats [1] [1] = 202;
+    flats [1] [2] = 203;
+
+    System.out.println("Printing a 2D array using for loop : ");
+    for (int i = 0; i < flats.length ; i++) {
+        for (int j = 0; j < flats[i].length ; j++) {
+            System.out.print(flats[i][j]);
+            System.out.print(" ");
+
+        }
+        System.out.println("");
+    }
 }
