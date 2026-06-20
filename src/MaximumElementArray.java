e6ca5dd055abc91e090a67c2d3d0e9e3a50324e5
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-20 16:43:06 +0530

    maximum element in array

---
+++ src/MaximumElementArray.java
@@ -0,0 +1,13 @@
+public class MaximumElementArray {
+    static void main() {
+        int [] arr = {1,22,4,2,66,73,21,31,86};
+        int max = 0;
+        for (int element : arr){
+            if (element>max){
+                max = element;
+
+            }
+        }
+        System.out.println("Maximum Number in Array is: "+ max);
+    }
+}
