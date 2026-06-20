1eae0aaafabe918aa5b41cc02febbe506f03f895
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-20 16:37:33 +0530

    ReversingArray

--- src/ReversingArray.java
+++ src/ReversingArray.java
@@ -1,7 +1,7 @@
 public class ReversingArray {
     static void main() {
         //Reversing an Array
-        int  [] arr = {1,2,3,4,5,6};
+        int  [] arr = {1,2,3,4,5,6,32,11};
         int l = arr.length;
         int a  = Math.floorDiv(l,2);
         int temp;
@@ -9,6 +9,7 @@ public class ReversingArray {
 
         for (int i = 0; i < a; i++) {
            //Swap a[i] and a[l-1-i]
+            //  a   b  temp
             // |4| |3| | |
             temp = arr[i];
             arr[i] = arr[l-i-1];
@@ -17,6 +18,8 @@ public class ReversingArray {
         for (int element : arr){
             System.out.println(element);
         }
+
+
 
 
     }
