cca52386a92b3e96f8ba0c1fc05de3e75ce73cc0
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-21 00:33:42 +0530

    good Night : )

--- src/VariableArguments.java
+++ src/VariableArguments.java
@@ -1,5 +1,10 @@
 public class VariableArguments {
-    static void main() {
 
+    static int sum(int a, int b){
+        return a+b;
+    }
+    static void main() {
+        System.out.println("LEARNING VarArgs ");
+        System.out.println("The sum of 4 & 5 is: "+ sum(4,5));
     }
 }
