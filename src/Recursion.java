7a23fb170eacdd6b9bff89b5fbab4b344dfeae7a
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-21 15:23:50 +0530

    Recursion with factorial example

---
+++ src/Recursion.java
@@ -0,0 +1,24 @@
+public class Recursion {
+
+    static int  factorial(int n){
+        // factorial(0) = 1
+        // factorial(n) = n* n-1 * n-2
+        // factorial(5) = 5 * 4 * 3 * 2 * 1 = 120
+        // LOGIC : factorial(n) = n* factorial(n-1)
+
+        if (n==0 || n==1){
+            return 1;
+        }else {
+            return n * factorial(n-1);
+        }
+
+    }
+
+    static void main() {
+
+        int n = 5;
+        System.out.println("The value of Factorial " + n + ": "+ factorial(n));
+
+    }
+
+}
