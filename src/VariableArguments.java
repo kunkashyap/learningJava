c51f64a0c0f69a86dff3b75b331d3ca4231a86a2
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-21 14:48:43 +0530

    varArgs

--- src/VariableArguments.java
+++ src/VariableArguments.java
@@ -1,10 +1,18 @@
 public class VariableArguments {
 
-    static int sum(int a, int b){
-        return a+b;
-    }
-    static void main() {
+        static int sum(int ...arr){
+            //Available as int [] arr
+            int result = 0;
+            for (int a : arr){
+                result += a;
+            }
+            return  result;
+         }
+
+
+        static void main() {
         System.out.println("LEARNING VarArgs ");
-        System.out.println("The sum of 4 & 5 is: "+ sum(4,5));
+        System.out.printf("The sum is: "+ sum(10,4,5,8,9));
+        System.out.println("The sum of nothing is: "+ sum());
     }
 }
