a614ed910121625333303666538c474295e2cf30
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-24 20:59:46 +0530

    recursion sum

--- src/PracticeOnMethods.java
+++ src/PracticeOnMethods.java
@@ -2,11 +2,47 @@ public class PracticeOnMethods {
     static void multiplication(int n){
         for (int i = 1; i <=10 ; i++) {
             System.out.format("%d X %d = %d \n",n,i,n*i);
-
         }
     }
+
+
+    static void pattern1(int n){
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j < i+1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    static int sumRect(int n){
+        //sum(3) = 3 + sum(2)
+        // sum(3) = 3 + 2 + sum(1)
+        // sum(3) = 3 + 2 + 1 (base condition)
+        if (n==1){  //Base Condition -> Recursion
+            return 1;
+        }else {
+            return n + sumRect(n-1);
+        }
+    }
+
+
+
+
     static void main(String[] args) {
-        //Problem-1
-        multiplication(6);
+//        //Problem-1
+//        multiplication(6);
+//
+//        //Problem-2
+//        pattern1(5);
+//
+//        //Problem-3 (Important - Recursion)
+//        int c;
+//        c =sumRect(3);
+//        System.out.println(c);
+
+
+
     }
 }
