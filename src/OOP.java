b7d612e60e9b665de924da083ef617eee05c3357
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-25 17:49:19 +0530

    object oriented programming

--- src/OOP.java
+++ src/OOP.java
@@ -7,4 +7,45 @@ public class OOP {
 
     //Class -Is a blueprint for creating objects
 
+
+/* OOP Terminology
+
+1. Abstraction - Hiding Internal Details [Show only essential info]
+               e.g. Using Phone without bothering about how it was made
+
+
+2. Encapsulation - The Act of Putting Various Components together (in
+                    a capsule)
+
+            e.g. Laptop is a single entity with Wi-Fi + speaker + camera +
+            storage in a single box
+
+
+            In Java, encapsulation simply means that the sensitive
+            data can be hidden from users
+
+
+3. Inheritance - The act of deriving new things from existing things
+            Rickshaw => E-Rickshaw
+            Phone => Smartphone
+
+            Implements DRY!
+
+
+
+
+4. Polymorphism - One entity many forms
+
+
+
+
+
+
+
+ */
+
+
+
+
+
 }
