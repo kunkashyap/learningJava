4f050a4b124b1eba5f5a8caa1b84360c73d41fb2
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-06-25 08:50:34 +0530

    printed pattern2 (pattern1 in reverse)

--- src/PracticeOnMethods.java
+++ src/PracticeOnMethods.java
@@ -27,6 +27,15 @@ public class PracticeOnMethods {
         }
     }
 
+    static void pattern2(int n){
+        for (int i = n; i > 0 ; i--) {
+            for (int j = 0; j < i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
 
 
 
@@ -43,6 +52,6 @@ public class PracticeOnMethods {
 //        System.out.println(c);
 
 
-
+            pattern2(4);
     }
 }
